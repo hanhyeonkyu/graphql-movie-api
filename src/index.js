9e70f3e457f8d606c32bc18ab6e70b6aa3e5ffa7
hanhyeonkyu <busgod1212@gmail.com>
2020-02-10 17:07:49 +0900

    make it better

--- src/index.js
+++ src/index.js
@@ -26,8 +26,7 @@ const options = {
     preflightContinue: false,
     optionsSuccessStatus: 204
   },
-  credentials: true,
-  tracing: "enabled"
+  credentials: true
 };
 
 const server = new GraphQLServer({
