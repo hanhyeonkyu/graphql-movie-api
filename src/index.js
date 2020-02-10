6fbc3510bacf3c265a0c2bdfa51bb8981f1cd228
hanhyeonkyu <busgod1212@gmail.com>
2020-02-10 15:55:15 +0900

    make it better

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
-const { makeExecutableSchema } = require('graphql-tools')
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
 
-const typeDefs = `
-type Movie {
-    id: Int!
-    title: String!
-    rating: Float
-    description_intro: String
-    language: String
-    medium_cover_image: String
-    genres: [String]
-  }
-`
 const server = new GraphQLServer({
   typeDefs: `type Movie {
     id: Int!
