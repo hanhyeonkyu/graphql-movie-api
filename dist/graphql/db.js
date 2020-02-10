77c276ed7bd04fda428df4df1c16ae86dd746909
hanhyeonkyu <busgod1212@gmail.com>
2020-02-11 08:54:04 +0900

    make it better

--- dist/graphql/db.js
+++ dist/graphql/db.js
@@ -5,13 +5,19 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.getSuggestions = exports.getMovie = exports.getMovies = undefined;
 
+var _regenerator = require("babel-runtime/regenerator");
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
 var _axios = require("axios");
 
 var _axios2 = _interopRequireDefault(_axios);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 var BASE_URL = "https://yts-proxy.now.sh/";
 var LIST_MOVIES_URL = BASE_URL + "list_movies.json";
@@ -19,10 +25,10 @@ var MOVIE_DETAILS_URL = BASE_URL + "movie_details.json";
 var MOVIE_SUGGESTIONS_URL = BASE_URL + "movie_suggestions.json";
 
 var getMovies = exports.getMovies = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(limit, rating) {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(limit, rating) {
     var _ref2, movies;
 
-    return regeneratorRuntime.wrap(function _callee$(_context) {
+    return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
@@ -53,10 +59,10 @@ var getMovies = exports.getMovies = function () {
 }();
 
 var getMovie = exports.getMovie = function () {
-  var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(id) {
+  var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(id) {
     var _ref4, movie;
 
-    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+    return _regenerator2.default.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
@@ -86,10 +92,10 @@ var getMovie = exports.getMovie = function () {
 }();
 
 var getSuggestions = exports.getSuggestions = function () {
-  var _ref5 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(id) {
+  var _ref5 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(id) {
     var _ref6, movies;
 
-    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+    return _regenerator2.default.wrap(function _callee3$(_context3) {
       while (1) {
         switch (_context3.prev = _context3.next) {
           case 0:
