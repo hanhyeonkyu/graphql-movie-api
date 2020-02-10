8c97114ebea03db62027c3b6ae3f5911ed533b41
hanhyeonkyu <busgod1212@gmail.com>
2020-02-10 16:09:28 +0900

    make it better

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,23 @@ var _resolvers2 = _interopRequireDefault(_resolvers);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var typeDefs = "\ntype Movie {\n  id: Int!\n  title: String!\n  rating: Float\n  description_intro: String\n  language: String\n  medium_cover_image: String\n  genres: [String]\n}\n\ntype Query {\n  movies(limit: Int, rating: Float): [Movie]!\n  movie(id: Int!): Movie\n  suggestions(id: Int!): [Movie]!\n}\n";
+
+var options = {
+  cors: {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  },
+  credentials: true
+};
+
 var server = new _graphqlYoga.GraphQLServer({
-  typeDefs: "type Movie {\n    id: Int!\n    title: String!\n    rating: Float\n    description_intro: String\n    language: String\n    medium_cover_image: String\n    genres: [String]\n  }\n  \n  type Query {\n    movies(limit: Int, rating: Float): [Movie]!\n    movie(id: Int!): Movie\n    suggestions(id: Int!): [Movie]!\n  }\n  ",
+  typeDefs: typeDefs,
   resolvers: _resolvers2.default
 });
 
-server.start(function () {
+server.start(options, function () {
   return console.log("Graphql Server Running");
 });
--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
 
+const typeDefs = `
+type Movie {
+  id: Int!
+  title: String!
+  rating: Float
+  description_intro: String
+  language: String
+  medium_cover_image: String
+  genres: [String]
+}
+
+type Query {
+  movies(limit: Int, rating: Float): [Movie]!
+  movie(id: Int!): Movie
+  suggestions(id: Int!): [Movie]!
+}
+`;
+
+const options = {
+  cors: {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  },
+  credentials: true
+};
+
 const server = new GraphQLServer({
-  typeDefs: `type Movie {
-    id: Int!
-    title: String!
-    rating: Float
-    description_intro: String
-    language: String
-    medium_cover_image: String
-    genres: [String]
-  }
-  
-  type Query {
-    movies(limit: Int, rating: Float): [Movie]!
-    movie(id: Int!): Movie
-    suggestions(id: Int!): [Movie]!
-  }
-  `,
+  typeDefs: typeDefs,
   resolvers
 });
 
-server.start(() => console.log("Graphql Server Running"));
+server.start(options, () => console.log("Graphql Server Running"));
