f57dd72a34bc657dfc015ec63143dfff3c111b63
hanhyeonkyu <busgod1212@gmail.com>
2020-02-10 17:19:02 +0900

    make it better

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -2,7 +2,13 @@ import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movies: (_, { rating, limit }) => {
+      try {
+        getMovies(limit, rating);
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
     movie: (_, { id }) => getMovie(Number(id)),
     suggestions: (_, { id }) => getSuggestions(Number(id))
   }
