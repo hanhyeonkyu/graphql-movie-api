ac51ba501102e9eed76724b52bcd76844d85ed77
hanhyeonkyu <busgod1212@gmail.com>
2020-02-11 13:40:06 +0900

    make it better

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -3,7 +3,7 @@ import { getMovies, getMovie, getSuggestions } from "./db";
 const resolvers = {
   Query: {
     test: (_, {}) => "********************************************",
-    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movies: async (_, { limit, rating }) => await getMovies(limit, rating),
     movie: (_, { id }) => getMovie(Number(id)),
     suggestions: (_, { id }) => getSuggestions(Number(id))
   }
