0cb9fdbdab5469d226bfbe6209ae90064870d5af
hanhyeonkyu <busgod1212@gmail.com>
2020-02-11 13:44:32 +0900

    make it better

--- dist/graphql/resolvers.js
+++ dist/graphql/resolvers.js
@@ -4,6 +4,14 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _regenerator = require("babel-runtime/regenerator");
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
 var _objectDestructuringEmpty2 = require("babel-runtime/helpers/objectDestructuringEmpty");
 
 var _objectDestructuringEmpty3 = _interopRequireDefault(_objectDestructuringEmpty2);
@@ -18,17 +26,38 @@ var resolvers = {
       (0, _objectDestructuringEmpty3.default)(_ref);
       return "********************************************";
     },
-    movies: function movies(_, _ref2) {
-      var limit = _ref2.limit,
-          rating = _ref2.rating;
-      return (0, _db.getMovies)(limit, rating);
-    },
-    movie: function movie(_, _ref3) {
-      var id = _ref3.id;
+    movies: function () {
+      var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(_, _ref3) {
+        var limit = _ref3.limit,
+            rating = _ref3.rating;
+        return _regenerator2.default.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return (0, _db.getMovies)(limit, rating);
+
+              case 2:
+                return _context.abrupt("return", _context.sent);
+
+              case 3:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, undefined);
+      }));
+
+      return function movies(_x, _x2) {
+        return _ref2.apply(this, arguments);
+      };
+    }(),
+    movie: function movie(_, _ref4) {
+      var id = _ref4.id;
       return (0, _db.getMovie)(Number(id));
     },
-    suggestions: function suggestions(_, _ref4) {
-      var id = _ref4.id;
+    suggestions: function suggestions(_, _ref5) {
+      var id = _ref5.id;
       return (0, _db.getSuggestions)(Number(id));
     }
   }
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -3,7 +3,12 @@ import { getMovies, getMovie, getSuggestions } from "./db";
 const resolvers = {
   Query: {
     test: (_, {}) => "********************************************",
-    movies: async (_, { limit, rating }) => await getMovies(limit, rating),
+    // movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movies: (_, { limit, rating }) => {
+      return getMovies(limit, rating).then(res => {
+        return res;
+      });
+    },
     movie: (_, { id }) => getMovie(Number(id)),
     suggestions: (_, { id }) => getSuggestions(Number(id))
   }
