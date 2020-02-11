ead225b8c28da13479c1c4434c83e13912ddf007
hanhyeonkyu <busgod1212@gmail.com>
2020-02-11 09:52:10 +0900

    make it better

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+axios.defaults.timeout = 1000;
 const BASE_URL = "https://yts-proxy.now.sh/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
