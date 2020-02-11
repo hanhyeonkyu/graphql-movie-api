8d5d2d8a0ce7c82b52cfde8d8c952b696e52e89f
hanhyeonkyu <busgod1212@gmail.com>
2020-02-11 14:04:51 +0900

    make it better

--- dist/graphql/db.js
+++ dist/graphql/db.js
@@ -26,7 +26,8 @@ var MOVIE_SUGGESTIONS_URL = BASE_URL + "movie_suggestions.json";
 
 var getMovies = exports.getMovies = function () {
   var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(limit, rating) {
-    var data, movies;
+    var _ref2, movies;
+
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -41,8 +42,8 @@ var getMovies = exports.getMovies = function () {
             });
 
           case 2:
-            data = _context.sent;
-            movies = data.data.data.movies;
+            _ref2 = _context.sent;
+            movies = _ref2.data.data.movies;
 
             if (!movies) {
               _context.next = 8;
@@ -68,8 +69,8 @@ var getMovies = exports.getMovies = function () {
 }();
 
 var getMovie = exports.getMovie = function () {
-  var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(id) {
-    var _ref3, movie;
+  var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(id) {
+    var _ref4, movie;
 
     return _regenerator2.default.wrap(function _callee2$(_context2) {
       while (1) {
@@ -83,8 +84,8 @@ var getMovie = exports.getMovie = function () {
             });
 
           case 2:
-            _ref3 = _context2.sent;
-            movie = _ref3.data.data.movie;
+            _ref4 = _context2.sent;
+            movie = _ref4.data.data.movie;
 
             if (!movie) {
               _context2.next = 8;
@@ -105,13 +106,13 @@ var getMovie = exports.getMovie = function () {
   }));
 
   return function getMovie(_x3) {
-    return _ref2.apply(this, arguments);
+    return _ref3.apply(this, arguments);
   };
 }();
 
 var getSuggestions = exports.getSuggestions = function () {
-  var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(id) {
-    var _ref5, movies;
+  var _ref5 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(id) {
+    var _ref6, movies;
 
     return _regenerator2.default.wrap(function _callee3$(_context3) {
       while (1) {
@@ -125,8 +126,8 @@ var getSuggestions = exports.getSuggestions = function () {
             });
 
           case 2:
-            _ref5 = _context3.sent;
-            movies = _ref5.data.data.movies;
+            _ref6 = _context3.sent;
+            movies = _ref6.data.data.movies;
 
             if (!movies) {
               _context3.next = 8;
@@ -147,6 +148,6 @@ var getSuggestions = exports.getSuggestions = function () {
   }));
 
   return function getSuggestions(_x4) {
-    return _ref4.apply(this, arguments);
+    return _ref5.apply(this, arguments);
   };
 }();
