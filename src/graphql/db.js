6b1f32372ec7d823332d3248cebe9c1306dc6fd5
hanhyeonkyu <busgod1212@gmail.com>
2020-02-11 12:46:54 +0900

    make it better

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -11,7 +11,7 @@ export const getMovies = async (limit, rating) => {
       limit,
       minimum_rating: rating
     },
-    baseUrl = 'http://your-app-name.herokuapp.com'
+    baseUrl = 'https://graphql-movieapi.herokuapp.com/'
   });
   console.log("------------------------------------");
   const movies = data.data.data.movies;
